f3a8e679a325d0ffff73867f5e1b79155dd4b635
Md Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2021-06-09 00:58:23 +0600

    complete navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <div>
+            <nav className="bg-gray-800 ">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="flex items-center justify-between h-16">
+                        <div className="flex items-center">
+                            <div className="flex-shrink-0">
+                                <img
+                                    className="h-8 w-8"
+                                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                                    alt="Workflow"
+                                />
+                            </div>
+                            <div className="hidden md:block">
+                                <div className="ml-10 flex items-baseline space-x-4 content-end">
+                                    <a
+                                        href="#t"
+                                        className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+                                    >
+                                        Dashboard
+                                    </a>
+
+                                    <a
+                                        href="#t"
+                                        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                    >
+                                        Team
+                                    </a>
+
+                                    <a
+                                        href="#t"
+                                        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                    >
+                                        Projects
+                                    </a>
+
+                                    <a
+                                        href="#t"
+                                        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                    >
+                                        Calendar
+                                    </a>
+
+                                    <a
+                                        href="#t"
+                                        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                    >
+                                        Reports
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Nav;
