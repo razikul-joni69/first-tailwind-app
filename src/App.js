8a352efd5aa55bc800d03bdf46b86cfcc854c6f9
Md Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2021-06-09 11:06:30 +0600

    make hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from "../src/components/NavBar/NavBar.jsx";
 import "./App.css";
+import HeroSection from "./components/HeroSection/HeroSection.jsx";
 
 function App() {
     return (
         <div>
           <NavBar/>
+          <HeroSection/>
         </div>
     );
 }
